8a0688cdbb9fb749ee6b554d8aedb6e022bbfd12
Asger Schultz <s183912@student.dtu.dk>
2020-02-11 19:24:03 +0100

    rustne tests

--- src/pyrs/src/lib.rs
+++ src/pyrs/src/lib.rs
@@ -5,4 +5,14 @@ pub extern "C" fn doub(x: i32) -> i32 {
     x * 2
 }
 
+#[no_mangle]
+pub extern "C" fn sum(x: Vec<i32>) -> i32 {
+    println!("{:?}", x);
+    let mut s = 0;
+    for elem in x {
+        s += elem;
+    }
+    s + 1
+}
+
 
--- src/ruster.py
+++ src/ruster.py
@@ -1,10 +1,20 @@
+import os, sys
+os.chdir(sys.path[0])
 from cffi import FFI
+import numpy as np
 
 ffi = FFI()
 ffi.cdef("""
 	int doub(int);
+	int sum(int *);
 """)
 
 C = ffi.dlopen("pyrs/target/debug/libpyrs.so")
+print(C.doub(5))
 
-print(C.doub(9))
+a = np.ones(40).astype(int) * 0
+print(sys.getsizeof(a))
+dat = a.__array_interface__["data"][0]
+cptr = ffi.cast("int*", dat)
+print(cptr)
+print(C.sum(cptr))
