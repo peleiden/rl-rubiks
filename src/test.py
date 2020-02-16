33c7fa80099260f1b027df324feb7f8ac5136ff7
Asger Schultz <s183912@student.dtu.dk>
2020-02-16 23:35:58 +0100

    Test af overførselstid til gpu

--- src/test.py
+++ src/test.py
@@ -1,24 +1,40 @@
 from utils.ticktock import TickTock
 import numpy as np
 from rubiks import RubiksCube
+import matplotlib.pyplot as plt
 import torch
+from pprint import pprint
 
 cpu = torch.device("cpu")
 gpu = torch.device("cuda")
 
-def to_cuda(a):
-	return torch.from_numpy(a).cuda()
-
-def to_cpu(a):
-	return a.cpu().numpy()
+def gen_data(size, dtype):
+	return torch.randint(0, 1, size=(int(size),), dtype=dtype)
 
 if __name__ == "__main__":
-
-	rube = RubiksCube()
+	sizes = torch.logspace(1, 4, 32)
+	dtypes = {torch.int8: 1, torch.int16: 2, torch.int32: 4, torch.int64: 8}
+	transfertimes = {str(dtype): [[], []] for dtype in dtypes}
 	tt = TickTock()
-	tt.tick()
-	a = to_cuda(rube.state)
-	tt.tock()
-	tt.tick()
-	b = to_cpu(a)
-	tt.tock()
+	for dtype in dtypes:
+		print(dtype)
+		for size in sizes:
+			nbytes = dtypes[dtype] * size
+			dat = gen_data(size, dtype)
+			dat.to(gpu)
+			tt.tick()
+			dat.to(gpu)
+			time = tt.tock(False)
+			transfertimes[str(dtype)][0].append(nbytes)
+			transfertimes[str(dtype)][1].append(time)
+	pprint(transfertimes)
+	
+	for dtype, time in transfertimes.items():
+		plt.plot(time[0], time[1], label=dtype)
+		plt.legend(loc=2)
+	plt.xlabel("Number of bytes")
+	plt.ylabel("Transfer time [s]")
+	plt.grid(True)
+	plt.show()
+
+
