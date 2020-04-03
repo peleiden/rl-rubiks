0a8a4a848dd7cb73ba094f69ef569757ddb2f582
Asger Schultz <asger.s@protonmail.com>
2020-04-03 21:08:53 +0200

    Implementeret node i MCTS-tr??

--- src/rubiks/solving/search.py
+++ src/rubiks/solving/search.py
@@ -5,13 +5,29 @@ from dataclasses import dataclass
 from src.rubiks.cube.cube import Cube
 from src.rubiks.utils.ticktock import TickTock
 
-@dataclass
 class Node:
 	state: np.ndarray
 	N: np.ndarray
 	W: np.ndarray
 	L: np.ndarray
 	P: np.ndarray
+	def __init__(self, state, value: float, policy: np.ndarray, from_node=None, action_idx: int=None):
+		self.state = state
+		self.value = value
+		self.P = policy
+		# self.neighs[i] is a tuple containing the state obtained by the action Cube.action_space[i]
+		# Tuples are used, so they can be used for lookups
+		self.neighs = [None] * 12
+		self.N = np.ones(12)
+		self.W = np.empty(12)
+		self.L = np.zeros(12)
+		if action_idx is not None:
+			from_action_idx = action_idx + 1 if action_idx % 2 == 0 else action_idx - 1
+			self.neighs[from_action_idx] = tuple(from_node.state)
+			self.W[from_action_idx] = from_node.value
+
+
+
 
 
 class Searcher:
@@ -41,13 +57,11 @@ class MCTS(Searcher):
 	def __init__(self, *args, **kwargs):
 		self.states = dict()
 		super().__init__(*args, **kwargs)
-		self.state_count = defaultdict(lambda: np.zeros(Cube.action_dim))
-		self.state_maxval = defaultdict(lambda: np.zeros(Cube.action_dim))
-		self.state_policy = dict()
 
 
 	def search(self, state: np.ndarray, time_limit: int):
 		tt = TickTock()
 		tt.tick()
+		self.states = Node(state, )
 		while tt.tock() < time_limit:
 			pass
