17c2d4594f4e37f7ad1cf2329c1448dbb01a3a02
Asger Schultz <asger.s@protonmail.com>
2020-04-07 22:27:45 +0200

    Tidspilde virker igen

--- src/project_management/line_counter.py
+++ src/project_management/line_counter.py
@@ -4,32 +4,61 @@ from pathlib import Path
 import git
 import matplotlib.pyplot as plt
 import numpy as np
+from collections import deque, defaultdict
+
+def exclude(d: str):
+	exclude_patterns = ["local", "node_modules", "dist", ".idea", "__pycache__", ".git"]
+	for pattern in exclude_patterns:
+		if pattern in d or f"/{pattern}" in d:
+			return True
+	return False
+
+def get_files(patterns: dict):
+	files = defaultdict(list)
+	dirs = lambda x: next(os.walk(x))[1]
+	q = deque(dirs("."))
+	while q:
+		v = q.popleft()
+		for d in dirs(v):
+			d = os.path.join(v, d)
+			if exclude(d): continue
+			q.append(d)
+			for f in os.listdir(d):
+				for ext in (x[0] for x in patterns.values()):
+					if f.lower().endswith(ext):
+						files[ext].append(os.path.join(d, f))
+	return files
+			
+
 repopath = os.path.realpath(os.path.join(os.getcwd(), "..", ".."))
 os.chdir(repopath)
 print(repopath)
 repo = git.Repo(repopath)
 commits = list(reversed([str(x) for x in repo.iter_commits()]))
 patterns = {
-	".py": ("*.[pP][yY]", "#"),
-	".ts": ("*.[tT][sS]", "//"),
-	".tex": ("*.[tT][eE][xX]", "%"),
-	".md": ("*.[mM][dD]", "None"*10),
+	".py": (".py", "#"),
+	".ts": (".ts", "//"),
+	".tex": (".tex", "%"),
+	".md": (".md", "None"*10),
 }
 n_commits = np.arange(0, len(commits)+1)
 n_lines = {kw: np.zeros(len(commits)+1) for kw in patterns}
+
+# files = get_files(patterns)
+# [print(len(x)) for x in files.values()]
+
 for i, commit in enumerate(commits):
 	cmd = f"git checkout {commit}"
 	print(f"{i+1} / {len(commits)} >>> {cmd}")
 	os.system(cmd)
-	for kw, (pattern, comment) in patterns.items():
-		files = list(Path(".").rglob(pattern))
-		files = [x for x in files if "/local" not in str(x) and "node_modules" not in str(x)]
-		for p in files:
+	files = get_files(patterns)
+	for ext, fs in files.items():
+		for p in fs:
 			with open(str(p)) as f:
 				lines = [x.strip() for x in f.readlines()]
 				for line in lines:
-					if line and not line.startswith(comment):
-						n_lines[kw][i+1] += 1
+					if line and not line.startswith(patterns[ext][1]):
+						n_lines[ext][i+1] += 1
 
 os.system("git checkout master")
 
