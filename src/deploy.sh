005c5534bc694c4caf8a92d80103fbb31dd12da7
Asger Schultz <s183912@student.dtu.dk>
2020-04-13 00:23:29 +0200

    Rettet deploy til ikke at bruge prod-branch
    Former-commit-id: 849f000c8a9a4f78d5311058474a8801275f1519

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,8 +1,4 @@
 cd "$(dirname "$0")"
 cd frontend
 ng deploy --base-href https://asgerius.github.io/rl-rubiks/
-git checkout prod
-git merge master
-git push
-git checkout master
 git push heroku master
