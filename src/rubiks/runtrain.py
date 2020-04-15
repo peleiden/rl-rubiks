642a1bc7ac57801888a54e3155d0297666b1dbbf
sorenmulli <swholm@protonmail.com>
2020-04-15 13:46:35 +0200

    Rewriting parser
    Former-commit-id: 3a26fb2152c826695160e8d6b9b4ea6496da8d6a

--- src/rubiks/runtrain.py
+++ src/rubiks/runtrain.py
@@ -1,15 +1,13 @@
 import sys, os
 
 from ast import literal_eval
-from argparse import ArgumentParser
-from configparser import ConfigParser
-from pprint import pformat
 
 import numpy as np
 import torch
 
 from src.rubiks.solving import search
 from src.rubiks.utils import seedsetter
+from src.rubiks.utils.parse import Parser
 from src.rubiks.utils.ticktock import get_timestamp
 from src.rubiks.utils.logger import Logger
 from src.rubiks import cpu, gpu, get_repr, set_repr, store_repr, restore_repr
@@ -18,28 +16,95 @@ from src.rubiks.train import Train
 from src.rubiks.solving.evaluation import Evaluator
 from src.rubiks.solving.agents import DeepAgent
 
-defaults  = {
-	'rollouts': 1000,
-	'location': 'data/local_train'+get_timestamp(for_file=True),
-	'rollout_games': 1000,
-	'rollout_depth': 100,
-	'batch_size': 50,
-	'loss_weighting': 'weighted',
-	'lr': 1e-5,
-	'optim_fn': 'RMSprop',
-	'searcher': 'MCTS',
-	'evaluations': 20,
-	'train_eval_games': 150,
-	'eval_max_time': 60,
-	'eval_scrambling': '10 25',
-	'final_evals': 10000,
-	'is2024': True,
+options = {
+	'location': {
+		'default':  'data/local_train'+get_timestamp(for_file=True),
+		'help':	    "Save location for logs and plots",
+		'type':	    str,
+	},
+	'rollouts': {
+		'default':  1000,
+		'help':	    'Number of passes of ADI+parameter update',
+		'type':	    int,
+	},
+	'rollout_games': {
+		'default':  1000,
+		'help':	    'Number of games in ADI in each rollout',
+		'type':	    int,
+	},
+	'rollout_depth': {
+		'default':  100,
+		'help':	    'Number of scramblings applied to each game in ADI',
+		"type":	    int,
+	},
+	'loss_weighting': {
+		'default':  'weighted',
+		'help':	    'Weighting method applied to scrambling depths',
+		'type':	    str,
+		'choices':  ['weighted', 'none', 'adaptive'],
+	},
+	'batch_size': {
+		'default':  50,
+		'help':	    'Number of training examples to be used in each parameter update',
+		'type':	    int
+	},
+	'lr': {
+		'default':  1e-5,
+		'help':	    'Learning rate of parameter update',
+		'type':	    float,
+	},
+	'optim_fn': {
+		'default':  'RMSprop',
+		'help':	    'Name of optimization function corresponding to class in torch.optim',
+		'type':	    str,
+	},
+	'searcher': {
+		'default':  'MCTS',
+		'help':	    'Name of searcher for agent corresponding to deepseacher class in src.rubiks.solving.search',
+		'type':	    str,
+		'choices':  ['MCTS', 'PolicySearch'],
+	},
+	'evaluations': {
+		'default':  20,
+		'help':	    'Number of evaluations during training. Some settings for these hard coded',
+		'type':	    int,
+	},
+	'train_eval_games': {
+		'default':  150,
+		'help':	    'Number of games used for each evaluation during training',
+		'type':	    int,
+	},
+	'final_evals': {
+		'default':  10000,
+		'help':	    'Number of games in final evaluation',
+		'type':	    int,
+	},
+	'eval_max_time': {
+		'default':  60,
+		'help':	    'Max searching time for agent in the FINAL evaluation',
+		'type':	    int,
+	},
+	'eval_scrambling': {
+		'default':  '10 25',
+		'help':	    'Two space-seperated integers (given in string delimeters such as --eval_scrambling "10 20") denoting interval'
+			    ' of number of scramblings to be run in final evaluation. In eval during training, the mean of these is used',
+		#Ugly way to define list of two numbers
+		'type':	    lambda args: [int(args.split()[0]), int(args.split()[1])],
+	},
+	'is2024': {
+		'default':  True,
+		'help':	    'True for 20x24 Rubiks representation and Flase for 6x8x6',
+		'type':	    literal_eval,
+		'choices':  [True, False],
+	},
 }
+
+
 
 class TrainJob:
 	def __init__(self,
-			jobname: str,
-			# Set by parser, should correspond to values in `default` above and defaults can be controlled there
+			name: str,
+			# Set by parser, should correspond to values in `options`  above and defaults can be controlled there
 			location: str,
 			rollouts: int,
 			rollout_games: int,
@@ -61,8 +126,8 @@ class TrainJob:
 			model_cfg: ModelConfig = ModelConfig(batchnorm=False),
 			max_train_eval_time = 1,
 		):
-		self.jobname = jobname
-		assert isinstance(self.jobname, str)
+		self.name = name
+		assert isinstance(self.name, str)
 
 		self.rollouts = rollouts
 		assert self.rollouts > 0
@@ -99,15 +164,15 @@ class TrainJob:
 		assert self.max_train_eval_time > 0
 
 		self.location = location
-		self.logger = Logger(f"{self.location}/{self.jobname}.log", jobname, verbose) #Already creates logger at init to test whether path works
-		self.logger.log(f"Initialized {self.jobname}")
+		self.logger = Logger(f"{self.location}/{self.name}.log", name, verbose) #Already creates logger at init to test whether path works
+		self.logger.log(f"Initialized {self.name}")
 
 		self.is2024 = is2024
 		self.model_cfg = model_cfg
 		assert isinstance(self.model_cfg, ModelConfig)
 
 	def execute(self):
-		self.logger(f"Starting job:\n{self.jobname}")
+		self.logger(f"Starting job:\n{self.name}")
 		store_repr()
 		set_repr(self.is2024)
 
@@ -157,79 +222,24 @@ class TrainJob:
 
 		return train.train_rollouts, train.train_losses
 
-def parse(defaults: dict):
-	# First off: A seperate parser for the configuration file
-	config_parser = ArgumentParser(add_help = False)
-	config_parser.add_argument('--config', help="Location of config file", metavar="FILE")
-	args, remaining_args = config_parser.parse_known_args()
-
-	#Parser for the rest of the options is added
-	parser = ArgumentParser(
-			description = 'Start one or more training sessions of the DeepCube agent using config or CLI arguments. If multiple runs are specified in config AND CLI arguments are given, CLI arguments overwrite settings for all runs',
-	parents = [config_parser])
-
-	parser.add_argument('--rollouts', help="Number of rollouts: Number of passes of ADI+parameter update", type=int)
-	parser.add_argument('--location', help="Save location for logs and plots", type=str)
-	parser.add_argument('--rollout_games', help="Number of games in ADI in each rollout", type=int)
-	parser.add_argument('--rollout_depth', help="Number of scramblings applied to each game in ADI", type=int)
-	parser.add_argument('--loss_weighting', help="Weighting of different scrambling depths", type=str, choices=['adaptive', 'weighted', 'none'])
-	parser.add_argument('--batch_size', help="Number of training examples to be used at the same time in parameter update", type=int)
-	parser.add_argument('--lr', help="Learning rate of parameter update", type=float)
-	parser.add_argument('--optim_fn', help="String corresponding to a class in torch.optim", type=str)
-	parser.add_argument('--searcher', help="String corresponding to a deepsearcher class in src.rubiks.solving.search", type=str, choices = ["MCTS", "PolicySearch",])
-	parser.add_argument('--evaluations', help="Number of evaluations (each consisting of 1/4 og rollout_games) to be done during training", type=int)
-	parser.add_argument('--train_eval_games', help="Number of games used for each evaluation during training", type=int)
-	parser.add_argument('--eval_max_time', help="Max time (seconds) for each game for the agent", type=int)
-	intlist_validator = lambda args: [int(args.split()[0]), int(args.split()[1])] #Ugly way to define list of two numbers
-	parser.add_argument('--eval_scrambling', help="Two space-seperated integers (given in string delimeters, such as --eval scrambling '10 20') denoting interval of number of scramblings to be run in evaluation. In evaluation during training, the mean of these is used", type=intlist_validator)
-	parser.add_argument('--final_evals', help="Number of games to be done in the evaluation after the training", type=int)
-	bool_validator = lambda b: literal_eval(b)
-	parser.add_argument('--is2024', help="True for 20x24 representation and False for 6x8x6", type=bool_validator, choices=[True, False])
-
-	jobs = list()
-	with_config = False
-	if args.config:
-		with_config = True
-		config = ConfigParser()
-		if not config.read([args.config]):
-			raise FileNotFoundError(f"Could not find config file {args.config}")
-
-		# If user sets a DEFAULT section, then this overwrites the defaults
-		defaults = {**defaults, **dict(config.items("DEFAULT"))}
-
-		# Each section corresponds to a job
-		for jobname in config.sections():
-			options = {**defaults, **dict(config.items(jobname))}
-			parser.set_defaults(**options)
-
-			job_args = parser.parse_args(remaining_args)
-			save_location = job_args.location #Save for dump at the end
-			job_args.location = f"{job_args.location}/{jobname.lower()}" # Give unique location to each run
-			del job_args.config
-			jobs.append(TrainJob(jobname, **vars(job_args)))
-
-	# If no config was added or config of only defaults were  added, run from CLI/defaults.
-	if (not args.config) or (not config.sections()):
-		parser.set_defaults(**defaults)
-		args = parser.parse_args(remaining_args)
-		save_location = args.location
-		del args.config
-		jobs.append(TrainJob("Training", **vars(args)))
-
-	# For reproduceability: Save config file and arguments
-	if with_config:
-		with open(f"{save_location}/used_config.ini", 'w', encoding="utf-8") as f: config.write(f)
-
-	with open(f"{save_location}/used_config.ini", 'a', encoding="utf-8") as f:
-		f.write(f"# Run command\n# {' '.join(sys.argv)}\n")
-		defs = pformat(defaults).replace('\n', '\n# ')
-		f.write(f"\n# All configuration values\n# {defs}\n")
-	return jobs
 
 if __name__ == "__main__":
+	description = r"""
 
+___________________________________________________________________
+  /_/_/_/\	______ _      ______ _   _______ _____ _   __ _____
+ /_/_/_/\/\	| ___ \ |     | ___ \ | | | ___ \_   _| | / //  ___|
+/_/_/_/\/\/\	| |_/ / |     | |_/ / | | | |_/ / | | | |/ / \ `--.
+\_\_\_\/\/\/	|    /| |     |    /| | | | ___ \ | | |    \  `--. \
+ \_\_\_\/\/	| |\ \| |____ | |\ \| |_| | |_/ /_| |_| |\  \/\__/ /
+  \_\_\_\/	\_| \_\_____/ \_| \_|\___/\____/ \___/\_| \_/\____/
+__________________________________________________________________
+Start one or more Reinforcement Learning training session(s)
+on the Rubik's Cube using config or CLI arguments.
+ """
 	# SET SEED
 	seedsetter()
-	jobs = parse(defaults)
+	parser = Parser(options, description=description)
+	jobs = [TrainJob(**settings) for settings in  parser.parse()]
 	for job in jobs:
 		job.execute()
