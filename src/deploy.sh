2b3782ba81bc61b76433048eaf49baf02d2bd2ee
Asger Schultz <s183912@student.dtu.dk>
2020-04-23 12:16:57 +0200

    Delvis api-omlægning
    Former-commit-id: 1adada22bf8fb96cd3b430c40a907e80af2c3d5e

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,4 +1,7 @@
 cd "$(dirname "$0")"
 cd frontend
 ng deploy --base-href https://asgerius.github.io/rl-rubiks/
-git push heroku master
+git checkout gh-pages
+git merge master
+git push
+git checkout master
