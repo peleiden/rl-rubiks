64d8f18f8283f18e75f4e6f59f948b089614f675
Asger Schultz <s183912@student.dtu.dk>
2020-04-23 12:28:43 +0200

    Opdateret deployfil
    Former-commit-id: 45bda5214e5f166580b5de4df52ce3b3dc25fb4f

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,7 +1,7 @@
 cd "$(dirname "$0")"
 cd frontend
 ng deploy --base-href https://asgerius.github.io/rl-rubiks/
-git checkout gh-pages
+git checkout prod
 git merge master
 git push
 git checkout master
