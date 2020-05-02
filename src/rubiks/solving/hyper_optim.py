962134473dff1c1133111c94df2d4b9ff4f4679b
sorenmulli <swholm@protonmail.com>
2020-05-02 16:22:58 +0200

    Finish BO class. Ready for use
    Former-commit-id: 301f9dc7e3b4fefb3ae61793ba4e004512abfd52

--- src/rubiks/solving/hyper_optim.py
+++ src/rubiks/solving/hyper_optim.py
@@ -43,17 +43,19 @@ class Optimizer:
 	def optimize(self, iterations: int):
 		raise NotImplementedError("To be implemented in child class")
 
-	def objective_from_evaluator(self, evaluator: Evaluator, searcher_class, persistent_searcher_params: dict, agent_class = DeepAgent):
+	def objective_from_evaluator(self, evaluator: Evaluator, searcher_class, persistent_searcher_params: dict, param_prepper: Callable=lambda x: x,  agent_class = DeepAgent):
 		self.evaluator = evaluator
 		self.searcher_class = searcher_class
 		self.agent_class = agent_class
 		self.persistent_searcher_params = persistent_searcher_params
 
-		def target_function(self, searcher_params):
-			searcher = self.seacher_class(**self.persistent_searcher_params, **seacher_params)
+		def target_function(searcher_params):
+			param_prepper(searcher_params)
+			searcher = self.searcher_class(**self.persistent_searcher_params, **searcher_params)
 			agent = self.agent_class(searcher)
 			res = self.evaluator.eval(agent)
-			return (res != -1).mean()
+			won = res != -1
+			return won.mean() if won.any() else 0
 
 		self.target_function = target_function
 
@@ -76,7 +78,6 @@ class BayesianOptimizer(Optimizer):
 			acquisition: str='ei',
 
 			logger: Logger=NullLogger(),
-			verbose: bool=True,
 
 		):
 		"""Set op BO class, set up utility function (acqusition function) and gaussian process.
@@ -88,45 +89,47 @@ class BayesianOptimizer(Optimizer):
 
 		self.optimizer = BayesianOptimization(
 			f=None,
-			pbounds=dict,
+			pbounds=parameters,
 			verbose=0,
 		)
-		self.optimizer.set_gp_params(alpha=alpha, n_restarts=n_restarts)
+		self.optimizer.set_gp_params(alpha=alpha, n_restarts_optimizer=n_restarts)
 		self.utility = UtilityFunction(kind=acquisition, kappa=2.5, xi=0)
 
-		self.logger(f"Created Bayesian Optimizer with alpha={alpha} and {n_restarts} for each optimization. Acquisition function is {acquisition}.")
+		self.logger(f"Created Bayesian Optimizer with alpha={alpha} and {n_restarts} restarts for each optimization. Acquisition function is {acquisition}.")
 
 	def optimize(self, iterations: int):
-		for i in self.iterations:
-			next_params = optimizer.suggest(self.utility)
+		for i in range(iterations):
+			next_params = self.optimizer.suggest(self.utility)
 			self.parameter_history.append(next_params)
-			self.logger("Optimization {i}: Chosen parameters:\t: {self._format_params(next_params)}")
+			self.logger(f"Optimization {i}: Chosen parameters:\t: {self._format_params(next_params)}")
 
-			score = self.target_function(**next_params)
+			score = self.target_function(next_params)
 			self.score_history.append(score)
-			self.logger("Optimization {i}: Score: {score}")
+			self.logger(f"Optimization {i}: Score: {score}")
 
 			self.optimizer.register(params=next_params, target=score)
 
-		high_idx = np.argmax(self.scores)
-		self.highscore = self.scores[high_idx]
+		high_idx = np.argmax(self.score_history)
+		self.highscore = self.score_history[high_idx]
 		self.optimal = self.parameter_history[high_idx]
 
-		self.logger("Optimization done. Best parameters: {self.optimal} with score {self.highscore}")
+		self.logger(f"Optimization done. Best parameters: {self._format_params(self.optimal)} with score {self.highscore}")
 
 		return self.optimal
 	def __str__(self):
 		return f"BayesianOptimizer()"
 
 def MCTS_optimize():
-	#Lot of overhead just default argument niceness: latest model is latest
+	#Lot of overhead just for default argument niceness: latest model is latest
 	from src.rubiks.runeval import train_folders
+
 	model_path = ''
 	if train_folders:
 		for folder in [train_folders[-1]] + glob(f"{train_folders[-1]}/*/"):
 				if os.path.isfile(os.path.join(folder, 'model.pt')):
 					model_path  = os.path.join(folder)
 					break
+
 	parser = argparse.ArgumentParser(description='Optimize Monte Carlo Tree Search for one model')
 	parser.add_argument('--location', help='Location for model.pt. Results will also be saved here',
 		type=str, default=model_path)
@@ -135,9 +138,12 @@ def MCTS_optimize():
 	args = parser.parse_args()
 
 	params = {
-		'c': (0.2, 0.8),
+		'c': (0.1, 1),
 		'nu': (0, 0.01),
+		'workers': (1, 200),
 	}
+	def prepper(params): params['workers'] = int(params['workers'])
+
 	persistent_params = {
 		'net' : Model.load(args.location),
 		'complete_graph': False,
@@ -145,10 +151,11 @@ def MCTS_optimize():
 	}
 	logger = Logger(os.path.join(args.location, 'optimizer.log'), 'Optimization')
 
+
 	evaluator = Evaluator(n_games=20, max_time=1, scrambling_depths=range(12, 20))
 	optimizer = BayesianOptimizer(target_function=None, parameters=params, logger=logger)
-	optimizer.objective_from_evaluator(evaluator, MCTS, persistent_params, persistent_searcher_params=persistent_params)
-	optimizer.optimize(parser.iterations)
+	optimizer.objective_from_evaluator(evaluator, MCTS, persistent_params, param_prepper=prepper)
+	optimizer.optimize(args.iterations)
 
 if __name__ == '__main__':
 	MCTS_optimize()
