bf34d994ea9a42f8c308136fb5b91a805f3c5e66
sorenmulli <swholm@protonmail.com>
2020-05-09 15:08:53 +0200

    Pythonpathg added in test setup
    Former-commit-id: ebcf6504a595cb595b13985349be1089d8c4abdc

--- src/tests/__init__.py
+++ src/tests/__init__.py
@@ -7,7 +7,8 @@ class MainTest:
 	def setup_class(cls):
 		os.makedirs("local_tests", exist_ok = True)
 		seedsetter()
-
+		repo_path = os.path.join( os.path.dirname(os.path.dirname(os.path.abspath(__file__))) )
+		if repo_path not in os.environ['PYTHONPATH']: os.environ['PYTHONPATH'] += f':{repo_path}'
 	@classmethod
 	def teardown_class(cls):
 		rmtree('local_tests', onerror=cls.ignore_absentee)
--- src/tests/test_runeval.py
+++ src/tests/test_runeval.py
@@ -12,7 +12,7 @@ class TestRuneval(MainTest):
 		#TODO: Add test with deepsearcher
 		run_settings = {'location': location, 'searcher': 'BFS', 'games': 2, 'max_time': 1, 'scrambling': '2 4',
 				'mcts_c': 0.6123, 'mcts_nu':.005, 'mcts_graph_search': False, 'policy_sample': True}
-		args = [sys.executable, run_path]
+		args = [sys.executable, run_path,]
 		for k,v in run_settings.items(): args.extend([f'--{k}', str(v)])
 		subprocess.check_call(args) #Raises error on problems in call
 
