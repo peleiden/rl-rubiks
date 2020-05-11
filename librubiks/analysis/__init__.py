Fixed target value calculation
Former-commit-id: 2b7a28c072aff18eba98f8c4ecd2f26ff84a5372

librubiks/analysis/__init__.py
@@ -22,7 +22,7 @@ class TrainAnalysis:
 
 		self.games = games
 		self.depth = depth
-		self.depths = np.arange(1, depth)
+		self.depths = np.arange(depth)
 		extra_evals = min(evaluations[-1] if len(evaluations) else 0, extra_evals) #Wont add evals in the future (or if no evals are needed)
 		self.evaluations = np.unique( np.append(evaluations, range( extra_evals )) )
 
@@ -65,11 +65,8 @@ class TrainAnalysis:
 			net.eval()
 
 			# Calculating value targets
-			targets = value_targets.cpu().numpy()
-			self.avg_value_targets.append(np.empty_like(self.depths, dtype=float))
-			for i, depth in enumerate(self.depths):
-				idcs = np.arange(self.games) * self.depth + depth
-				self.avg_value_targets[-1][i] = targets[idcs].mean()
+			targets = value_targets.cpu().numpy().reshape((-1, self.depth))
+			self.avg_value_targets.append(targets.mean(axis=0))
 
 			# Calculating model change
 			model_change = torch.sqrt((net.get_params()-self.params)**2).mean().cpu()
@@ -81,6 +78,7 @@ class TrainAnalysis:
 			#TODO: Calculate value given to first 12 substates
 
 			net.train()
+
 	def ADI(self, values: torch.Tensor):
 		"""Saves statistics after a run of ADI. """
 		self.substate_val_stds.append(

librubiks/train.py
@@ -305,7 +305,7 @@ class Train:
 		values = values.reshape(-1, 12)
 		policy_targets = torch.argmax(values, dim=1)
 		value_targets = values[np.arange(len(values)), policy_targets]
-		#value_targets[solved_scrambled_states] = 0  # Max Lapan's convergence fix
+		value_targets[solved_scrambled_states] = 0  # Max Lapan's convergence fix
 		self.tt.end_profile("Calculating targets")
 
 		weighted = np.tile(1 / np.arange(1, self.rollout_depth+1), self.rollout_games)