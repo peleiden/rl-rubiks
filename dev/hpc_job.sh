f6a22a71b91ea6f135855cabf60b446b14c0293b
sorenmulli <swholm@protonmail.com>
2020-05-11 08:29:01 +0200

    Set up default time on jobscript
    Former-commit-id: 8fb475ddf89f8ac2490c56ffbcfa46bf8d84de6f

--- dev/hpc_job.sh
+++ dev/hpc_job.sh
@@ -1,18 +1,1 @@
-#!/bin/sh
-#BSUB -q gpuv100
-#BSUB -gpu "num=1"
-#BSUB -J "Training_procedure"
-#BSUB -R "rusage[mem=10GB]"
-#BSUB -n 1
-#BSUB -W 8:00
-#BSUB -o logs/%J.out
-#BSUB -e logs/%J.err
-#BSUB -u s183911@student.dtu.dk
-#BSUB -R "select[gpu16gb]"
-#BSUB -N
-
-echo "Running job"
-python3 runtrain.py --config data/configs/hpc_train.ini
-python3 runeval.py --config data/configs/hpc_eval.ini
-echo "Finished running job"
-
+#BSUB -W 15:00
