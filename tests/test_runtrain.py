522f0d4871fb292f447badf6b31a035fbac725b9
sorenmulli <swholm@protonmail.com>
2020-05-20 13:44:13 +0200

    Fix test bugs introduced in 8efeb0e
    Former-commit-id: 27edef9ab663a486f25e283b7056a915a7ea4010

--- tests/test_runtrain.py
+++ tests/test_runtrain.py
@@ -9,13 +9,13 @@ class TestRuntrain(MainTest):
 		run_path = os.path.join( os.path.dirname(os.path.dirname(os.path.abspath(__file__))),  'runtrain.py' )
 		location = 'local_tests/train'
 		run_settings = {'location': location, 'rollouts': 1, 'rollout_games': 2, 'rollout_depth':2, 'batch_size':2,
-				'alpha_update': 0.5, 'lr': 0.1, 'optim_fn': 'Adam', 'evaluation_interval': 0, 'is2024': True, 'arch': 'fc', 'analysis': True, 'nn_init': '1.2312312412e-4'}
+				'alpha_update': 0.5, 'lr': 0.1, 'optim_fn': 'Adam', 'evaluation_interval': 2, 'is2024': True, 'arch': 'fc', 'analysis': True, 'nn_init': '1.2312312412e-4'}
 		args = [sys.executable, run_path]
 		for k,v in run_settings.items(): args.extend([f'--{k}', str(v)])
 		subprocess.check_call(args)  # Raises error on problems in call
 
 		#TODO Add all files
-		expected_files = ['model.pt', 'train.log',  'config.json',  'model-min.pt', 'training.png', 'substate_dists.png']
+		expected_files = ['model.pt', 'train.log',  'config.json',  'model-best.pt', 'training.png', 'substate_dists.png']
 		expected_train_data_files = ['rollouts.npy','losses.npy']
 		print(os.listdir(location))
 		print(os.listdir(os.path.join(location, 'train-data')))
