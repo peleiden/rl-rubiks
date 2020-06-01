dad9fcbfdd9d79c40c08e56cfac62e7e7a1d8415
Asger Schultz <as@optoceutics.com>
2020-06-01 15:57:46 +0200

    Benchmarking now uses much less memory

--- librubiks/analysis/benchmark.py
+++ librubiks/analysis/benchmark.py
@@ -6,6 +6,15 @@ from librubiks.utils import Logger, TickTock
 def _repstr():
 	return "20x24" if get_is2024() else "6x8x6"
 
+def _get_states(shape: tuple):
+	shape = (*shape, *cube.shape()) if len(shape) > 1 else (1, *shape, *cube.shape())
+	states = np.empty(shape, dtype=cube.dtype)
+	states[0] = cube.repeat_state(cube.get_solved(), shape[1])
+	for i in range(1, len(states)):
+		faces, dirs = np.random.randint(0, 6, shape[1]), np.random.randint(0, 2, shape[1], dtype=bool)
+		states[i] = cube.multi_rotate(states[i-1], faces, dirs)
+	return states
+
 class CubeBench:
 
 	def __init__(self, log: Logger, tt: TickTock):
@@ -25,7 +34,7 @@ class CubeBench:
 	
 	def multi_rotate(self, n: int, n_states: int):
 		self.log.section(f"Benchmarking {TickTock.thousand_seps(n)} multi rotations of "
-						 f"{TickTock.thousand_seps(n_states)} states, {_repstr()}")
+						 f"{TickTock.thousand_seps(n_states)} states each, {_repstr()}")
 		states = cube.repeat_state(cube.get_solved(), n_states)
 		faces, dirs = np.random.randint(0, 6, (n, n_states)), np.random.randint(0, 2, (n, n_states))
 		pname = f"{TickTock.thousand_seps(n_states)} rotations, {_repstr()}"
@@ -37,7 +46,7 @@ class CubeBench:
 	
 	def onehot(self, n: int):
 		self.log.section(f"Benchmarking {TickTock.thousand_seps(n)} one-hot encodings, {_repstr()}")
-		states, _ = cube.sequence_scrambler(1, n, True)
+		states = _get_states((n,))
 		pname = f"One-hot encoding single state, {_repstr()}"
 		for state in states.squeeze():
 			self.tt.profile(pname)
@@ -47,9 +56,8 @@ class CubeBench:
 	
 	def multi_onehot(self, n: int, n_states: int):
 		self.log.section(f"Benchmarking {TickTock.thousand_seps(n)} one-hot encodings of "
-				 		 f"{TickTock.thousand_seps(n_states)} states, {_repstr()}")
-		all_states, _ = cube.sequence_scrambler(n_states, n, True)
-		all_states = all_states.reshape((n_states, n, *cube.shape())).transpose(1, 0, *(np.arange(len(cube.shape()))+2))
+				 		 f"{TickTock.thousand_seps(n_states)} states each, {_repstr()}")
+		all_states = _get_states((n, n_states))
 		pname = f"One-hot encoding {TickTock.thousand_seps(n_states)} states, {_repstr()}"
 		for states in all_states:
 			self.tt.profile(pname)
@@ -70,7 +78,7 @@ def benchmark():
 	cube_bench = CubeBench(log, tt)
 
 	# Cube config variables
-	cn = int(1e5)
+	cn = int(1e6)
 	multi_op_size = 1000  # Number of states used in multi operations
 
 	store_repr()
