83b253e12be0384440de17771de2293d511dd3f9
Soren Winkel Holm <swholm@protonmail.com>
2020-06-01 21:05:06 +0200

    Location in dev jobscript is nicer when more configurable

--- dev/hpc_job.sh
+++ dev/hpc_job.sh
@@ -4,14 +4,14 @@
 #BSUB -J "Training_procedure"
 #BSUB -R "rusage[mem=10GB]"
 #BSUB -n 1
-#BSUB -W 15:00
+#BSUB -W 23:00
 #BSUB -o logs/%J.out
 #BSUB -e logs/%J.err
 #BSUB -u s183911@student.dtu.dk
-#BSUB -R "select[gpu16gb]"
+#BSUB -R "select[gpu32gb]"
 #BSUB -N
 
 echo "Running job"
-python3 runtrain.py --config configs/hpc_train.ini
-python3 runeval.py --config configs/hpc_eval.ini
+python3 runtrain.py --config configs/hpc_train.ini --location data/local_hpc_train
+python3 runeval.py --config configs/hpc_eval.ini --location data/local_hpc_train
 
