2bc2694e2bbcd8b55021837a8dd3bdc3a433a74b
Soren Winkel Holm <swholm@protonmail.com>
2020-06-15 17:01:16 +0200

    Add agent evaluation config and remove old examples

--- dev/hpc_job.sh
+++ dev/hpc_job.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 #BSUB -q gpuv100
-#BSUB -gpu "num=1"
+#BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -J "Training_procedure"
 #BSUB -R "rusage[mem=10GB]"
 #BSUB -n 1
-#BSUB -W 23:00
+#BSUB -W 16:00
 #BSUB -o logs/%J.out
 #BSUB -e logs/%J.err
 #BSUB -u s183911@student.dtu.dk
 #BSUB -R "select[gpu32gb]"
 #BSUB -N
 
+##
 echo "Running job"
-python3 runtrain.py --config configs/hpc_train.ini --location data/local_hpc_train
-python3 runeval.py --config configs/hpc_eval.ini --location data/local_hpc_train
+# python3 runtrain.py --config configs/main_train.ini --location data/mainextra --rollouts 100
+python3 runeval.py --config configs/main_eval.ini --location ../rubiks-models/methodexperiment
+
+#python3 runtrain.py --config configs/method_train.ini --location data/methodexperiment4
+#python3 librubiks/solving/hyper_optim.py --location ../rubiks-models/main --agent EGVM
 
