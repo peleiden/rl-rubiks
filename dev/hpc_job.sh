cac001f57e86f071b8b24a97724ae6952ea7aa60
Soren Winkel Holm <swholm@protonmail.com>
2020-06-24 11:06:05 +0200

    Job script cleanup Â#27

--- dev/hpc_job.sh
+++ dev/hpc_job.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #BSUB -q gpuv100
-#BSUB -gpu "num=1:mode=exclusive_process"
+#BSUB -gpu "num=1"
 #BSUB -J "Training_procedure"
 #BSUB -R "rusage[mem=10GB]"
 #BSUB -n 1
@@ -11,11 +11,6 @@
 #BSUB -R "select[gpu32gb]"
 #BSUB -N
 
-##
 echo "Running job"
-# python3 runtrain.py --config configs/main_train.ini --location data/mainextra --rollouts 100
-python3 runeval.py --config configs/main_eval.ini --location ../rubiks-models/methodexperiment
-
-#python3 runtrain.py --config configs/method_train.ini --location data/methodexperiment4
-#python3 librubiks/solving/hyper_optim.py --location ../rubiks-models/main --agent EGVM
-
+python runtrain.py --config configs/train_ex.ini --location ../rubiks-models/main
+python runeval.py --config configs/eval_ex.ini --location ../rubiks-models/main
